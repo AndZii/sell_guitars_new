34f5da7b21ae3f83cf0c5de10e50d99f4d35aad6
AndreiZykov <zykovwat2008@gmail.com>
2017-01-04 23:56:17 -0500

    recapcha fix

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-config.site_key = '6LdoFw8UAAAAAFr-16_Ydc9JjYpPEDwlmAbeEUxX' 
-config.secret_key = '6LdoFw8UAAAAAPNLLeEMW0qyzGE_SMzsao-7O1IG' 
+config.site_key = '6LdOqRAUAAAAALGfqk2RNXa-Ed6c84e2kXsidiQV' 
+config.secret_key = '6LdOqRAUAAAAABrAANHtH0n3T-tFu0ERAlwCgL2_' 
 end
